430d15be99ef9a3c56aebbe1d451275e21cc0930
Anik Rigaud <s@tanas.net>
2016-08-28 12:25:24 +0200

    mini modif routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,25 @@
 Rails.application.routes.draw do
-  # get 'attendances/create'
-
-  # get 'sessions/index'
-
-  # get 'sessions/create'
-
-  # get 'sessions/new'
-
-  # get 'sessions/edit'
-
-  # get 'sessions/show'
-
-  # get 'sessions/update'
-
-  # get 'sessions/destroy'
-
-  # get 'memberships/index'
-
-  # get 'memberships/create'
-
-  # get 'memberships/new'
-
-  # get 'memberships/edit'
-
-  # get 'memberships/show'
-
-  # get 'memberships/update'
-
-  # get 'memberships/destroy'
-
-  # get 'users/index'
-
-  # get 'users/create'
-
-  # get 'users/new'
-
-  # get 'users/edit'
-
-  # get 'users/show'
-
-  # get 'users/update'
-
+  # get 'attendances/create
+  # get 'sessions/index
+  # get 'sessions/create
+  # get 'sessions/new
+  # get 'sessions/edit
+  # get 'sessions/show
+  # get 'sessions/update
+  # get 'sessions/destroy
+  # get 'memberships/index
+  # get 'memberships/create
+  # get 'memberships/new
+  # get 'memberships/edit
+  # get 'memberships/show
+  # get 'memberships/update
+  # get 'memberships/destroy
+  # get 'users/index
+  # get 'users/create
+  # get 'users/new
+  # get 'users/edit
+  # get 'users/show
+  # get 'users/update
   # get 'users/destroy'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
