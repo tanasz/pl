2c470b915cefdea63727e6d92f4c5668f3325269
Anik Rigaud <s@tanas.net>
2016-08-29 11:27:26 +0200

    modifs homepage et controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @message = current_user.nil? ? "Please login" : "Welcome back"
     @datetime = Date.today
+  end
+
+  def stats
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  get 'trainings/index'
+
+  get 'trainings/create'
+
+  get 'trainings/new'
+
+  get 'trainings/destroy'
+
+  get 'trainings/update'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  get 'profile', to: 'users#show'
   root to: 'pages#home'
+  get 'profile', to: 'users#show'
+  get 'stats', to: 'pages#stats'
   scope '(:locale)', locale: /fr|en/ do
     resources :memberships
     resources :users, except: [:show]
