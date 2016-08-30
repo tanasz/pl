66ebe3e49eb23c567f8616ddf7c98f29ece2b7d3
Anik Rigaud <s@tanas.net>
2016-08-30 08:19:39 +0200

    minibug navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,14 @@
 Rails.application.routes.draw do
-  get 'trainings/index'
-
-  get 'trainings/create'
-
-  get 'trainings/new'
-
-  get 'trainings/destroy'
-
-  get 'trainings/update'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'pages#home'
   get 'profile', to: 'users#show'
   get 'stats', to: 'pages#stats'
   scope '(:locale)', locale: /fr|en/ do
-    resources :memberships
-    resources :users, except: [:show]
+    resources :memberships, except: [:create, :destroy]
+    resources :users, only: [:new, :show, :index, :edit]
     resources :trainings do
-      resources :attendances, only: [:show, :create]
+      resources :attendances, only: [:show, :new, :index, :edit], shallow: true
     end
   end
 end
